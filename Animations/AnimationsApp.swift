33747bf10b145a35d032f54e25d79a2406a5cd67
yokobuto <info@yokobuto.de>
2025-12-18 16:58:19 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jörg Klausewitz on 18.12.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
